551e13274c01a985bee4602696c1cb4ca273232f
pepitoenlared <41177027+pepitoenlared@users.noreply.github.com>
2018-07-13 12:38:07 -0400

    Add files via upload

---
+++ BO/PersonBO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace BO
+{
+    public abstract class PersonBO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public char Gender { get; set; }
+        //public DateTime Time { get; set; }
+        public string Time { get; set; }
+
+        public virtual void SetTime()
+        {
+            this.Time = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
+        }
+
+        public virtual void SetTime(string time)
+        {
+            //CultureInfo MyCultureInfo = new CultureInfo("de-DE");
+            //this.Time = DateTime.Parse(time, MyCultureInfo);
+            this.Time = DateTime.ParseExact(time, "yyyyMMddHHmmss", CultureInfo.InvariantCulture).ToString("dd/MM/yyyy HH:mm:ss");
+        }
+    }
+}
---
+++ BO/StudentBO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BO
+{
+    public class StudentBO : PersonBO
+    {
+        public TypeBO Type { get; set; }
+
+        //public override void SetTime()
+        //{
+        //    base.SetTime();
+        //}
+    }
+}
