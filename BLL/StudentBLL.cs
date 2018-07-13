2e2fdb1a5664d03487d6bebe938147b9377fc546
pepitoenlared <41177027+pepitoenlared@users.noreply.github.com>
2018-07-13 12:41:01 -0400

    Add files via upload

---
+++ BLL/StudentBLL.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BO;
+using DAL;
+
+namespace BLL
+{
+    public class StudentBLL
+    {
+        public bool Create (int typeId, string name, char gender)
+        {
+            return StudentDAL.Create(typeId, name, gender);
+        }
+
+        public bool Edit (int id, int typeId, string name, char gender)
+        {
+            return StudentDAL.Edit(id, typeId, name, gender);
+        }
+
+        public bool Delete (int id)
+        {
+            return StudentDAL.Delete(id);
+        }
+
+        public StudentBO findById (int id)
+        {
+            return StudentDAL.findById(id);
+        }
+
+        public List<StudentBO> getList()
+        {
+            if (StudentDAL.getList().Count == 0)
+                StudentDAL.LoadData();
+            return StudentDAL.getList();
+        }
+
+        public List<StudentBO> searchListByType(int typeId)
+        {
+            return StudentDAL.searchListByType(typeId);
+        }
+
+        public List<StudentBO> searchList(int typeId, char gender, string name)
+        {
+            return StudentDAL.searchList(typeId, gender, name.ToLower());
+        }
+    }
+}
---
+++ BLL/TypeBLL.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BO;
+using DAL;
+
+namespace BLL
+{
+    public class TypeBLL
+    {
+        public bool Insert(TypeBO type)
+        {
+            return TypeDAL.Insert(type);
+        }
+
+        public bool Edit(TypeBO type)
+        {
+            return TypeDAL.Edit(type);
+        }
+
+        public bool Delete(TypeBO type)
+        {
+            return TypeDAL.Delete(type);
+        }
+
+        public List<TypeBO> getList()
+        {
+            if (TypeDAL.getList().Count == 0)
+                TypeDAL.LoadData();
+
+            return TypeDAL.getList();
+        }
+
+        public List<TypeBO> getListSearch()
+        {
+            if (TypeDAL.getList().Count == 0)
+                TypeDAL.LoadData();
+
+            return TypeDAL.getListSearch();
+        }
+    }
+}
